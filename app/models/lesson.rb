ec7f31b875778ff845574d9a39561dd5a7dde8e1
Alan Dust <adust1@gmail.com>
2018-10-29 15:45:03 -0400

    added a user table and model

---
+++ app/models/lesson.rb
@@ -0,0 +1,6 @@
+class Lesson < ApplicationRecord
+
+
+  belongs_to :teacher, class_name: "Lesson"
+  belongs_to :student, class_name: "Lesson"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -18,13 +18,23 @@ ActiveRecord::Schema.define(version: 2018_10_19_204832) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "profile_photo"
+    t.integer "age", null: false
+    t.string "role", default: "student", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "students_id"
+    t.bigint "teachers_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["students_id"], name: "index_users_on_students_id"
+    t.index ["teachers_id"], name: "index_users_on_teachers_id"
   end
 
 end
